1404b933d30816b7225ec8ed1df4f6a5874ad4d7
Devin Singh <drsingh2518@icloud.com>
2024-07-25 23:30:35 -0400

    diskio_ram: fill in stub functions using mmio disk

--- source/diskio_ram.c
+++ source/diskio_ram.c
@@ -1,14 +1,28 @@
 #include "diskio_ram.h"
+#include "ffconf.h"
 
+volatile char *RAM_DISK_ADDR = (volatile char *)0xD0000000;
+
+// MMIO disk already initializes the disk
 DSTATUS RAM_disk_status() {
-    return -1;
+    return 0;
 }
+
+// MMIO disk already initializes the filesystem during initial
 DSTATUS RAM_disk_initialize() {
-    return -1;
+    return 0;
 }
+
 DRESULT RAM_disk_read(BYTE *buff, LBA_t sector, UINT count) {
-    return RES_ERROR;
+    for (unsigned int i = 0; i < (FF_MAX_SS * count); i++) {
+        buff[i] = RAM_DISK_ADDR[(sector * FF_MAX_SS) + i];
+    }
+    return RES_OK;
 }
+
 DRESULT RAM_disk_write(const BYTE *buff, LBA_t sector, UINT count) {
-    return RES_ERROR;
+    for (unsigned int i = 0; i < (FF_MAX_SS * count); i++) {
+        RAM_DISK_ADDR[(sector * FF_MAX_SS) + i] = buff[i];
+    }
+    return RES_OK;
 }
